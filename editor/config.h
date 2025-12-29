da6427d607f58e18681b575ad50bd5d65545f3ee
TomAndJerrys2 <kylev2005@hotmail.com>
2025-12-29 18:47:35 +0000

    Switched off Canonical mode for upcoming buffer storage for lines and characters

--- editor/config.h
+++ editor/config.h
@@ -2,9 +2,11 @@
 #define CONFIG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define float f16
 #define unsigned float u16
@@ -15,6 +17,9 @@
 #define double d64
 #define unsigned double u64
 
+typedef struct termios editor;
+
 inline void enable_raw_input(void);
+inline void disable_raw_input(void);
 
 #endif
--- editor/keditor.c
+++ editor/keditor.c
@@ -1,19 +1,27 @@
 #include "config.h"
 
 inline void enable_raw_input(void) {
-	struct termios raw_input;
-
-	tcgetattr(STDIN_FILENO, & raw_input);
-
+	tcgetattr(STDIN_FILENO, & editor);
+	atexit(disable_raw_input());
+	
+	struct termios raw_input = editor;
 	// c_lflag is a field for local flags
 	// similar to flag fields like c_iflag
 	// and c_oflag which are i/o flags
-	raw.c_lflag &= ~(ECHO);
+	//
+	// ICANON lets us turn of canonical mode
+	// which allows us to read from byte to byte
+	// instead of from line to line
+	raw.c_lflag &= ~(ECHO | ICANON);
 
 	tcsetatttr(STDIN_FILENO, TCSAFLUSH, & raw_input);
 }
 
-i32 main() {
+inline void disable_raw_input(void) {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, & editor);
+}
+
+i32 main(void) {
 	
 	enable_raw_input();
 
@@ -23,7 +31,12 @@ i32 main() {
 	// Read the result of the character
 	// -- q --> quit
 	while(read(STDIN_FILENO, & input, 1) == true
-			&& input != 'q');
+			&& input != 'q') {
+		if(iscntrl(input)) 
+			printf("%d\n", input);
+		else 
+			printf("%d [%c] \n", input, input);
+	}
 
 	return EXIT_SUCCESS;
 }
